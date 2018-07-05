18bde5706685d630fc03dbe64ee710bb367e40b8
Julian beck <ju-fa-beck@t-online.de>
2018-07-05 09:11:04 +0900

    Added tests for  playlist and folder

--- test/test.js
+++ test/test.js
@@ -69,7 +69,7 @@ describe('converterFunctions', () => {
 
 
 describe('Full Tests', async function () {
-    this.timeout(100000);
+    this.timeout(10000000);
     it('Download youtube video', async function () {
         let option = {
             output: testFolder,
@@ -86,4 +86,84 @@ describe('Full Tests', async function () {
         expect(result).to.be.an("array");
         expect(result).to.have.all.members(output);
     });
+    it('Download playlist ', async function () {
+        rimraf.sync(testFolder + "/YouTube");
+        let option = {
+            output: testFolder,
+            cover: true,
+            metadata: true,
+            rename: true
+        }
+        let output = ['Mediator Design Pattern - Beau teaches JavaScript_00.00-03.00.mp3',
+            'Mediator Design Pattern - Beau teaches JavaScript_03.00-05.08.mp3',
+            'Module Design Pattern - Beau teaches JavaScript_00.00-02.44.mp3',
+            'Observer Design Pattern - Beau teaches JavaScript_00.00-03.00.mp3',
+            'Observer Design Pattern - Beau teaches JavaScript_03.00-03.56.mp3',
+            'Singleton Design Pattern - Beau teaches JavaScript_00.00-03.00.mp3',
+            'Singleton Design Pattern - Beau teaches JavaScript_03.00-04.50.mp3'
+        ]
+        await vt("https://www.youtube.com/playlist?list=PLWKjhJtqVAbnZtkAI3BqcYxKnfWn_C704", option);
+        let result = fs.readdirSync(testFolder + "/YouTube/audio");
+        expect(result).to.be.an("array");
+        expect(result).to.have.all.members(output);
+        rimraf.sync(testFolder + "/YouTube/audio");
+    });
+    it('Convert Folder', async function () {
+        let option = {
+            cover: true,
+            metadata: true,
+            rename: true
+        }
+        let output = ['Mediator Design Pattern - Beau teaches JavaScript_00.00-03.00.mp3',
+            'Mediator Design Pattern - Beau teaches JavaScript_03.00-05.08.mp3',
+            'Module Design Pattern - Beau teaches JavaScript_00.00-02.44.mp3',
+            'Observer Design Pattern - Beau teaches JavaScript_00.00-03.00.mp3',
+            'Observer Design Pattern - Beau teaches JavaScript_03.00-03.56.mp3',
+            'Singleton Design Pattern - Beau teaches JavaScript_00.00-03.00.mp3',
+            'Singleton Design Pattern - Beau teaches JavaScript_03.00-04.50.mp3'
+        ]
+        await vt(testFolder + "/YouTube", option);
+        let result = fs.readdirSync(testFolder + "/YouTube/audio");
+        expect(result).to.be.an("array");
+        expect(result).to.have.all.members(output);
+        rimraf.sync(testFolder + "/YouTube/audio");
+
+    });
+    it('Convert Regex', async function () {
+        let option = {
+            cover: true,
+            metadata: true,
+            rename: true
+        }
+        let output = ['Mediator Design Pattern - Beau teaches JavaScript_00.00-03.00.mp3',
+            'Mediator Design Pattern - Beau teaches JavaScript_03.00-05.08.mp3',
+            'Module Design Pattern - Beau teaches JavaScript_00.00-02.44.mp3',
+            'Observer Design Pattern - Beau teaches JavaScript_00.00-03.00.mp3',
+            'Observer Design Pattern - Beau teaches JavaScript_03.00-03.56.mp3',
+            'Singleton Design Pattern - Beau teaches JavaScript_00.00-03.00.mp3',
+            'Singleton Design Pattern - Beau teaches JavaScript_03.00-04.50.mp3'
+        ]
+        await vt(testFolder + "/YouTube/*Pattern*", option);
+        let result = fs.readdirSync(testFolder + "/YouTube/audio");
+        expect(result).to.be.an("array");
+        expect(result).to.have.all.members(output);
+        rimraf.sync(testFolder + "/YouTube/audio");
+    });
+    
+    it('Convert Single file', async function () {
+        let option = {
+            cover: true,
+            metadata: true,
+            rename: true
+        }
+        let output = ['Mediator Design Pattern - Beau teaches JavaScript_00.00-03.00.mp3',
+            'Mediator Design Pattern - Beau teaches JavaScript_03.00-05.08.mp3',
+        ]
+        await vt(testFolder + "/YouTube/Mediator Design Pattern - Beau teaches JavaScript.mp4", option);
+        let result = fs.readdirSync(testFolder + "/YouTube/audio");
+        expect(result).to.be.an("array");
+        expect(result).to.have.all.members(output);
+        rimraf.sync(testFolder + "/YouTube/audio");
+    });
+
 });
